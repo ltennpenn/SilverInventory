551b7026f3a45b7e683c9297b1d67544c5104af9
ltennpenn <56142460+ltennpenn@users.noreply.github.com>
2019-10-03 18:45:33 -0700

    Add files via upload
    Initial Commit "20191003_1845"

---
+++ Inventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silver_Inventory
+{
+    class Item
+    {
+        #region Properties
+        public string Type { get; set; }
+        public string Year { get; set; }
+        public string Condition { get; set; }
+        public string Description { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public string PurchasePrice { get; set; }
+        public string NurismaticValue { get; set; }
+        public int MeltValue { get; set; }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Silver_Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var item1 = new Item();
+            item1.Type = "American Silver Eagle";
+            item1.Year = "2019";
+            item1.Condition = "MS70";
+            item1.Description = "FDI";
+            item1.PurchaseDate = "201901151200";
+            item1.PurchasePrice = "45";
+            item1.NurismaticValue = "47";
+            item1.MeltValue = "17.68";
+        }
+    }
+}
